710705f32438bb0a10620a7d2d1379e06aa67e66
Matthew Brignola <matthewbrignola@ymail.com>
2022-01-11 21:24:09 -0700

    port declaration

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const url = require('url');
 // Helper method for generating unique ids
 const uuid = require('./helpers/uuid');
 
-const PORT = 3001;
+const PORT = process.env.port || 3001;
 
 const app = express();
 
